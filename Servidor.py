7773e9fcb12be194e6b325ef51e03c6d8b58b19a
Juan Daniel Ahumada <jd.ahumada10@uniandes.edu.co>
2019-09-29 23:16:04 -0500

    Cosas varias

--- Servidor.py
+++ Servidor.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-      
+"""
+Created on Tue Sep 24 07:16:39 2019
+
+@author: Juan Daniel
+"""
+
 import socket, threading
 import os
 import hashlib
@@ -115,134 +120,3 @@ while True:
 for t in threads:
     t.join()
     
-    """
-Created on Sun Sep 29 15:46:01 2019
-
-@author: Juan
-
-
-import socket
-import sys
-import git
-import datetime
-import hashlib                   # Import socket module
-
-port = 12000                    # Reserve a port for your service.
-s = socket.socket()             # Create a socket object
-host = socket.gethostname()     # Get local machine name
-s.bind((host, port))            # Bind to the port
-s.listen(2) 
-
-repoLocal = git.Repo( './' )
-x = datetime.datetime.now()
-nameFile="/Servidor/Logs/"+x+".txt"
-f= open(nameFile,"w+")                    # Now wait for client connection.
-
-print ('Server listening....')
-
-
-
-while True:
-    conn, addr = s.accept()     # Establish connection with client.
-    print ('Got connection from', addr)
-    data = conn.recv(5)
-    x = datetime.datetime.now()
-    f.write(x+"-Recibido listo conexion, ")
-    
-    print('Server received', repr(data))
-
-    filename='mytext.txt'
-    f = open(filename,'rb')
-    l = f.read(1024)
-    while (l):
-       conn.send(l)
-       print('Sent ',repr(l))
-       l = f.read(1024)
-    f.close()
-
-    print('Done sending')
-    conn.send('Thank you for connecting')
-    conn.close()
-    
-import socket
-import sys
-
-# Create a TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# Bind the socket to the port
-server_address = ('localhost', 15010)
-print (sys.stderr, 'starting up on %s port %s' % server_address)
-sock.bind(server_address)
-# Listen for incoming connections
-sock.listen(1)
-
-#Logs
-import logging
-
-
-
-# create logger
-module_logger = logging.getLogger('server_logger')
-
-class Auxiliary:
-    def __init__(self):
-        self.logger = logging.getLogger('server_logger.auxiliary.Auxiliary')
-        self.logger.info('creating an instance of Auxiliary')
-
-    def do_something(self):
-        self.logger.info('doing something')
-        a = 1 + 1
-        self.logger.info('done doing something')
-
-def some_function():
-    module_logger.info('received a call to "some_function"')
-
-# create logger with 'server-log'
-logger = logging.getLogger('server_logger')
-logger.setLevel(logging.DEBUG)
-# create file handler which logs even debug messages
-fh = logging.FileHandler('server.log')
-fh.setLevel(logging.DEBUG)
-# create console handler with a higher log level
-ch = logging.StreamHandler()
-ch.setLevel(logging.ERROR)
-# create formatter and add it to the handlers
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-fh.setFormatter(formatter)
-ch.setFormatter(formatter)
-# add the handlers to the logger
-logger.addHandler(fh)
-logger.addHandler(ch)
-
-logger.info('creating an instance of auxiliary_module.Auxiliary')
-a = Auxiliary()
-logger.info('created an instance of auxiliary_module.Auxiliary')
-logger.info('calling auxiliary_module.Auxiliary.do_something')
-a.do_something()
-logger.info('finished auxiliary_module.Auxiliary.do_something')
-logger.info('calling auxiliary_module.some_function()')
-some_function()
-logger.info('done with auxiliary_module.some_function()')
-#FINLOG
-
-while True:
-    # Wait for a connection
-    print (sys.stderr, 'waiting for a connection')
-    connection, client_address = sock.accept()
-    try:
-        print (sys.stderr, 'connection from', client_address)
-
-        # Receive the data in small chunks and retransmit it
-        while True:
-            data = connection.recv(16)
-            print (sys.stderr, 'received "%s"' % data)
-            if data:
-                print (sys.stderr, 'sending data back to the client')
-                connection.sendall(data)
-            else:
-                print (sys.stderr, 'no more data from', client_address)
-                break
-            
-    finally:
-        # Clean up the connection
-        connection.close()"""
