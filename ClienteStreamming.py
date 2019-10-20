02f9a55f69c2075c7a678dbf2cebbb854d2ea870
lala341 <li.forero@uniandes.edu.co>
2019-10-20 15:36:12 -0500

    Cambios

--- ClienteStreamming.py
+++ ClienteStreamming.py
@@ -1,21 +1,14 @@
+import pickle
 import socket
 import struct
-import pickle
+
 import cv2
 
-
-MCAST_GRP = '224.1.1.1'
-MCAST_PORT = 5007
+MCAST_GRP = "224.0.0.1"
+MCAST_PORT = 10000
 IS_ALL_GROUPS = True
-MULTICAST_TTL = 2
+
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-
-
-sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, MULTICAST_TTL)
-sock.sendto("robot", (MCAST_GRP, MCAST_PORT))
-
-
-
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 if IS_ALL_GROUPS:
     # on this port, receives ALL multicast groups
@@ -23,33 +16,26 @@ if IS_ALL_GROUPS:
 else:
     # on this port, listen ONLY to MCAST_GRP
     sock.bind((MCAST_GRP, MCAST_PORT))
+
 mreq = struct.pack("4sl", socket.inet_aton(MCAST_GRP), socket.INADDR_ANY)
+
 sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 
 
-data = b'' ### CHANGED
-payload_size = struct.calcsize("L") ### CHANGED
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((MCAST_GRP, MCAST_PORT))
+
+s=""
 
 while True:
+    print("I'm in while loop")
+    data, addr = sock.recvfrom(46080)
+    s+= data
+    if len(s) == (46080*20):
+        frame = numpy.fromstring (s, dtype=numpy.uint8)
+        frame = frame.reshape(480,640,3)
+        cv2.imshow("frame",frame)
 
-    # Retrieve message size
-    while len(data) < payload_size:
-        data += sock.recv(4096)
-
-    packed_msg_size = data[:payload_size]
-    data = data[payload_size:]
-    msg_size = struct.unpack("L", packed_msg_size)[0] ### CHANGED
-
-    # Retrieve all data based on message size
-    while len(data) < msg_size:
-        data += sock.recv(4096)
-
-    frame_data = data[:msg_size]
-    data = data[msg_size:]
-
-    # Extract frame
-    frame = pickle.loads(frame_data)
-
-    # Display
-    cv2.imshow('frame', frame)
-    cv2.waitKey(1)
+        s=""
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
